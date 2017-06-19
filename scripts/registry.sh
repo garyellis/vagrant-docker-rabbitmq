acffc962aa4393a570ad99b397dd51db111600f4
gary ellis <gary.luis.ellis@gmail.com>
2017-06-18 22:39:29 -0700

    removed catted text from script

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -24,30 +24,3 @@ docker run -d -p 5000:5000 --restart=always --name registry \
 
 mkdir -p /etc/docker/certs.d/$HOSTNAME:5000
 openssl s_client -connect $HOSTNAME:5000 2>/dev/null <<<""|sed -n '/-----BEGIN/,/-----END/p' > /etc/docker/certs.d/$HOSTNAME:5000/ca.crt
-[root@ci-server ~]# vim registry.sh
-[root@ci-server ~]# vim registry.sh
-[root@ci-server ~]# cat registry.sh
-#!/bin/bash
-
-mkdir -p /etc/registry/{auth,certs}
-
-docker run --entrypoint htpasswd registry:2 -Bbn user Welcome1  > /etc/registry/auth/htpasswd
-
-openssl req \
-       -newkey rsa:2048 -nodes -keyout /etc/registry/certs/$HOSTNAME.key \
-       -x509 -sha256 -days 365 -out /etc/registry/certs/$HOSTNAME.crt \
-       -subj "/C=US/ST=Arizona/L=Phoenix/O=devel/CN=$HOSTNAME"
-
-
-docker run -d -p 5000:5000 --restart=always --name registry \
-  -v /etc/registry:/etc/registry \
-  -e "REGISTRY_AUTH=htpasswd" \
-  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
-  -e REGISTRY_AUTH_HTPASSWD_PATH=/etc/registry/auth/htpasswd \
-  -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/registry/certs/$HOSTNAME.crt \
-  -e REGISTRY_HTTP_TLS_KEY=/etc/registry/certs/$HOSTNAME.key \
-  registry:2
-
-
-mkdir -p /etc/docker/certs.d/$HOSTNAME:5000
-openssl s_client -connect $HOSTNAME:5000 2>/dev/null <<<""|sed -n '/-----BEGIN/,/-----END/p' > /etc/docker/certs.d/$HOSTNAME:5000/ca.crt
